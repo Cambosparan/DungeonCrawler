33bd7aebeb8c8b5782179b3083bfef13849b9649
Ethan Young <eyoung5@mail.umw.edu>
2021-12-06 22:39:54 +0000

    Added different types of enemies

--- roguelike/EnemyGenerator.java
+++ roguelike/EnemyGenerator.java
@@ -4,10 +4,21 @@
 import java.util.Random;
 
 public class EnemyGenerator {
+
+	private static Random rng;
+
 	public static Enemy generate(int row, int col) {
-        // TODO: replace this with your own code!
-	
-	return new Enemy("Goblin", row, col, 20, 5, 3);
+		Random rng = new Random();
+		Enemy enemy = new Enemy("Enemy", row, col, 0, 0, 0);
+		int enemyPicker = rng.nextInt(3);
+		if (enemyPicker == 0) {
+			enemy = new Enemy("Draugr", row, col, 50, 8, 20);
+		}
+		else if (enemyPicker == 1) {
+			enemy = new Enemy("Restless Draugr", row, col, 150, 9, 0);
+		}
+		else if (enemyPicker == 2) {
+			enemy = new Enemy("Draugr Overlord", row, col, 210, 13, 20);
+		}
 	}
 }
-
