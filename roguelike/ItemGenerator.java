5c19c518b4000ee2f20edf4d44fc4cd2fbfe492b
Ethan Young <eyoung5@mail.umw.edu>
2021-12-06 19:37:00 +0000

    Added in our own Item Generator

--- roguelike/ItemGenerator.java
+++ roguelike/ItemGenerator.java
@@ -4,9 +4,69 @@
 import java.util.Random;
 
 public class ItemGenerator {
-    public static Item generate() {
-        // TODO: replace this with your own code!
-        return new Item(ItemType.Other, "Vase", 10, 2, 0);
-    }
-}
 
+	private static Random rng;
+
+	public static Item generate() {
+		Random rng = new Random();
+		Item item = new Item(ItemType.Other, "Item", 0, 0, 0);
+		int itemPicker = rng.nextInt(3);
+		if (itemPicker == 0) {
+			int weaponPicker = rng.nextInt(5);
+			if (weaponPicker == 0) {
+				item = new Item(ItemType.Weapon, "Steel Sword", 8, 10, 45);
+			}
+			else if (weaponPicker == 1) {
+				item = new Item(ItemType.Weapon, "Hunting Bow", 7, 7, 50);
+			}
+			else if (weaponPicker == 2) {
+				item = new Item(ItemType.Weapon, "Steel Dagger", 5, 3, 18);
+			}
+			else if (weaponPicker == 3) {
+				item = new Item(ItemType.Weapon, "Steel Greatsword", 17, 17, 90);
+			}
+			else if (weaponPicker == 4) {
+				item = new Item(ItemType.Weapon, "Dragonbone Warhammer", 28, 33, 4275);
+			}
+		}
+
+		if (itemPicker == 1) {
+			int armorPicker = rng.nextInt(5);
+			if (armorPicker == 0) {
+				item = new Item(ItemType.Armor, "Steel Horned Helmet", 17, 5, 125);
+			}
+			else if (armorPicker == 1) {
+				item = new Item(ItemType.Armor, "Steel Armor", 31, 35, 275);
+			}
+			else if (armorPicker == 2) {
+				item = new Item(ItemType.Armor, "Steel Cuffed Boots", 12, 8, 55);
+			}
+			else if (armorPicker == 3) {
+				item = new Item(ItemType.Armor, "Steel Nordic Gauntlets", 12, 4, 55);
+			}
+			else if (armorPicker == 4) {
+				item = new Item(ItemType.Armor, "Steel Shield", 24, 12, 150);
+			}
+		}
+
+		if (itemPicker == 2) {
+			int otherPicker = rng.nextInt(5);
+			if (otherPicker == 0) {
+				item = new Item(ItemType.Other, "Lockpick", 0, 0, 2);
+			}
+			else if (otherPicker == 1) {
+				item = new Item(ItemType.Other, "Wooden Plate", 0, 1, 1);
+			}
+			else if (otherPicker == 2) {
+				item = new Item(ItemType.Other, "Dragon Bone", 0, 15, 500);
+			}
+			else if (otherPicker == 3) {
+				item = new Item(ItemType.Other, "Golden Dragon Claw", 0, 1, 100);
+			}
+			else if (otherPicker == 4) {
+				item = new Item(ItemType.Other, "Steel Ingot", 0, 1, 20);
+			}
+		}
+		return item;
+	}
+}
