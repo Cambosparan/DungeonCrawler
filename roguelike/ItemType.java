370bf6dbaf2f5e2295fa7d51036fb727dfc94526
Ethan Young <eyoung5@mail.umw.edu>
2021-12-07 02:41:04 +0000

    Added the Javadoc documentation

--- roguelike/ItemType.java
+++ roguelike/ItemType.java
@@ -1,6 +1,7 @@
-// ItemType.java
-// an enum for specifying what type an item is
-// this is important when it comes to equipping
+/**This is the item type enumeration used by the ItemGenerator class
+ * There are a total of four different types that and item can be
+ * @author Ethan Young
+ */
 
 public enum ItemType {
     Weapon,
